d6d6f3ce08977ebce8cf2e7c15e3899b12020118
msenoussi <msenoussi@wanadev.fr>
2021-11-28 11:56:12 +0100

    eslint set up

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import i18n from "./services/i18n";
 import { store } from "./store";
 import BootstrapVue3 from 'bootstrap-vue-3'
-import Autocomplete from 'vue3-autocomplete'
 import 'vue3-autocomplete/dist/vue3-autocomplete.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
@@ -14,5 +13,4 @@ const app = createApp(App)
 app.use(i18n)
 app.use(store)
 app.use(BootstrapVue3)
-app.component('Autocomplete', Autocomplete)
 app.mount('#app')
